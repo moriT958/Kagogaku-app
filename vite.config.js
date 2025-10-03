0fa9fbca86ea866179554257773deca166df2a69
moriT958 <morit958@icloud.com>
2025-10-03 04:50:04 +0000

    feat: 開発用コンテナのセットアップ

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
+    port: 3000,
+    host: "0.0.0.0",
     proxy: {
       "/api": {
         target: "http://localhost:8080",
