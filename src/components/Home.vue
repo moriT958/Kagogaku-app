84b6910fef35e6669e1915832dea3d830e904b1c
akasuke26 <chiyanjunjie@gmail.com>
2025-10-05 06:11:37 +0000

    bb

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,53 +1,48 @@
 <script setup>
   import { ref } from 'vue';
   // import Train from './components/Train.vue'
-  const textInput=ref('');
+  const name=ref('');
   const goToTrain=()=>{
+    if(selectedIndex.value===null){
+      alert("キャラを選択してください")
+      return
+    }
+    if(name.value===''){
+      alert("名前を入力してください")
+      return
+    }
     window.location.hash='/Train'
   }
-  const saveImage = (url) => {
-    localStorage.setItem('c1', url)
-    // alert(`選択した画像を保存しました: ${url}`);
-  }
-  // const saveName = (cName) => {
-  //   localStorage.setItem('cn', cName)
-  // }
-  const images=document.querySelectorAll('.images img');
+  
+  const imageList=[
+    'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi_KDuCUJ4SXPSs6ZVz2LFbkWyS4XTccFEfiizRuoUPM9PmR1Ob9P7IJpFG4hb2_wloVK6eMLvN8bwv8ILauUQWacxn7DzeHCvuQudsNZucMUTrzaMdHRzwjBJ5pDVJNTMhwAnDegyKBtAs6wEjFgsc2HY-9q4WoCHVuBzYIQ_Xv1KPBK8ZBHyx9EpGX3PA/s180-c/bucket_boy.png',
+    'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjs8AvZeLXqN9PMHmzpxBpXq5033bksBQHA3PD35qpdKvH1Rs6E30OYoe0u4Bpch_7tv0cBy6jJLSOzCwDKDGUlrJybLhGyqlbWGe9wFE5_3i6ccR2C3TVj9Tq-rvd8P9CT7VT3aof5jBDP/s180-c/buranko_girl_sad.png',
+    'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhFEro64Nm55L1dVyHRAcfxmBk10XUp8LhnimtcACnq4y3NcQ2CL90khDUTdwXKhlAc4bSXDZ79Zuo8rLSazzCwH61-iuyQgTLCaDtb5Ol4kJfPvswy2qOa1lbO1-RiIA8rMf_VkuRdBnKQ/s180-c/sori_snow_boy.png'
+  ]
 
-  images.forEach(img=>{
-    img.addEventListener('click',()=>{
-      images.forEach(i=>i.classList.remove('selected'));
-      img.classList.add('selected');
-    });
-  });
+  const selectedIndex=ref(null)
+
+  function handleClick(img,index){
+    selectedIndex.value=index
+    localStorage.setItem('c1', img)
+    // alert(`選択した画像を保存しました: ${index}`);
+  }
 </script>
 
 <template>
   <p>好きなキャラを選んでね！</p>
   <div class="images">
-    <!-- 画像1 -->
-    <img 
-        src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi_KDuCUJ4SXPSs6ZVz2LFbkWyS4XTccFEfiizRuoUPM9PmR1Ob9P7IJpFG4hb2_wloVK6eMLvN8bwv8ILauUQWacxn7DzeHCvuQudsNZucMUTrzaMdHRzwjBJ5pDVJNTMhwAnDegyKBtAs6wEjFgsc2HY-9q4WoCHVuBzYIQ_Xv1KPBK8ZBHyx9EpGX3PA/s180-c/bucket_boy.png" 
-        alt="キャラ1"
-        @click="saveImage('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi_KDuCUJ4SXPSs6ZVz2LFbkWyS4XTccFEfiizRuoUPM9PmR1Ob9P7IJpFG4hb2_wloVK6eMLvN8bwv8ILauUQWacxn7DzeHCvuQudsNZucMUTrzaMdHRzwjBJ5pDVJNTMhwAnDegyKBtAs6wEjFgsc2HY-9q4WoCHVuBzYIQ_Xv1KPBK8ZBHyx9EpGX3PA/s180-c/bucket_boy.png')"
-      />
-      <!-- 画像2 -->
-      <img 
-        src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjs8AvZeLXqN9PMHmzpxBpXq5033bksBQHA3PD35qpdKvH1Rs6E30OYoe0u4Bpch_7tv0cBy6jJLSOzCwDKDGUlrJybLhGyqlbWGe9wFE5_3i6ccR2C3TVj9Tq-rvd8P9CT7VT3aof5jBDP/s180-c/buranko_girl_sad.png" 
-        alt="キャラ2"
-        @click="saveImage('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjs8AvZeLXqN9PMHmzpxBpXq5033bksBQHA3PD35qpdKvH1Rs6E30OYoe0u4Bpch_7tv0cBy6jJLSOzCwDKDGUlrJybLhGyqlbWGe9wFE5_3i6ccR2C3TVj9Tq-rvd8P9CT7VT3aof5jBDP/s180-c/buranko_girl_sad.png')"
-      />
-      <!-- 画像3 -->
-      <img 
-        src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhFEro64Nm55L1dVyHRAcfxmBk10XUp8LhnimtcACnq4y3NcQ2CL90khDUTdwXKhlAc4bSXDZ79Zuo8rLSazzCwH61-iuyQgTLCaDtb5Ol4kJfPvswy2qOa1lbO1-RiIA8rMf_VkuRdBnKQ/s180-c/sori_snow_boy.png" 
-        alt="キャラ3"
-        @click="saveImage('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhFEro64Nm55L1dVyHRAcfxmBk10XUp8LhnimtcACnq4y3NcQ2CL90khDUTdwXKhlAc4bSXDZ79Zuo8rLSazzCwH61-iuyQgTLCaDtb5Ol4kJfPvswy2qOa1lbO1-RiIA8rMf_VkuRdBnKQ/s180-c/sori_snow_boy.png')"
-      />
+    <img
+      v-for="(img,index) in imageList"
+      :key="index"
+      :src="img"
+      :class="{selected: selectedIndex === index}"
+      @click="handleClick(img,index)"
   </div>
   <p>キャラ名</p>
-  <input type="text" v-model="textInput" />
+  <input type="text" v-model="name" placeholder="名前を入力してください" />
   <p></p>
-  <button @click="goToTrain" class="start">はじめる</button>
+  <button @click="goToTrain(e)" class="start">はじめる</button>
 </template>
 
 <style>
@@ -65,7 +60,7 @@
   }
   
   input{
-    width:320px;
+    width:352px;
     height:32px;
     font-size:32px;
     background:white;
@@ -91,5 +86,6 @@
 
   .selected{
     filter:brightness(50%);
+    border: 4px solid #4caf50;
   }
 </style>
